5bc41dc84cdce312c32e74a07f07ad12b28f4438
Krzysiek Zuczkowski <krzysiek23@windowslive.com>
2022-12-29 22:09:38 +0100

    32.3 Thymeleaf email templates Once a day

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -18,14 +21,33 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://krzysiekzuczkowski.github.io");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("app_name", adminConfig.getAppName());
-        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("app_functionality", functionality);
+        context.setVariable("show_app_functionality", true);
         return templateEngine.process("mail/created-trello-card-mail.html", context);
     }
 
+    public String buildInformationEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://krzysiekzuczkowski.github.io");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", true);
+        context.setVariable("show_app_functionality", false);
+        return templateEngine.process("mail/created-trello-card-mail.html", context);
+    }
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -39,7 +39,11 @@ public class SimpleEmailService {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(mail.getMailTo());
             messageHelper.setSubject(mail.getSubject());
-            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+            messageHelper.setText(
+                    mail.getSubject().contains("Once a day email") ?
+                        mailCreatorService.buildInformationEmail(mail.getMessage()) :
+                        mailCreatorService.buildTrelloCardEmail(mail.getMessage())
+                    , true);
         };
     }
 
